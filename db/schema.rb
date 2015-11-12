6e049bd2bb83306f8034ed67f0d3493da648816c
Felipe Quintero <ffquint@gmail.com>
2015-11-12 14:40:18 -0800

    worked on code and removed unnecessary files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110224631) do
+ActiveRecord::Schema.define(version: 20151112212857) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
