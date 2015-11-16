ea1e8043b995120e31b1b0c4d0734fb364e9758c
Felipe Quintero <ffquint@gmail.com>
2015-11-16 11:14:48 -0800

    Assignment 34 - completed tasks 1 through 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  
+
+  get 'sponsored_posts_controller/show'
+
+  get 'sponsored_posts_controller/new'
+
+  get 'sponsored_posts_controller/edit'
+
   resources :topics do
     resources :posts, except: [:index]
+    resources :sponsoredposts
   end
 
   get 'about' => 'welcome#about'
