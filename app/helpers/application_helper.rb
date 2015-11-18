48fa8a383f258172a0b86fde1f8a4583e143aa9f
Felipe Quintero <ffquint@gmail.com>
2015-11-17 20:38:48 -0800

    Changing code to display gravatar image instead of a url, not successful

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       content_tag :div, capture(&block), class: 'form-group'
     end
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
