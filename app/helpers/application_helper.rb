787247ce006205d0dd97da19e4f802555a7b2c86
Felipe Quintero <ffquint@gmail.com>
2015-11-17 20:57:00 -0800

    Assignment 38 completed. Updated gravatar code and added css styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,5 @@ module ApplicationHelper
   def avatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
