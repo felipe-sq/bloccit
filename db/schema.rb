77cc140f2902674b22cc11ce1807c06ccc3e1757
Felipe Quintero <ffquint@gmail.com>
2015-11-23 14:56:41 -0800

    Checkpoint 43 completed.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122071639) do
+ActiveRecord::Schema.define(version: 20151123224355) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20151122071639) do
     t.integer  "topic_id"
     t.integer  "user_id"
     t.integer  "rating"
+    t.float    "rank"
   end
 
   add_index "posts", ["rating"], name: "index_posts_on_rating"
@@ -82,5 +83,16 @@ ActiveRecord::Schema.define(version: 20151122071639) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
