928eef0b77850cf50894240bc3ae61ce99ae2e17
Felipe Quintero <ffquint@gmail.com>
2015-11-24 18:55:58 -0800

    Added create and edit methods to ratings controller.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,13 @@
 class RatingsController < ApplicationController
+
+  def create
+    @rating = Rating.new(params[:severity])
+  end
+
+  def edit
+    @rating = self.update_rating(rating_string)
+  end
+
   def show
     @rating = Rating.find(params[:id])
   end
