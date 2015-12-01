1c8337154bc07047d41e43720cb499ede685c507
Felipe Quintero <ffquint@gmail.com>
2015-12-01 13:53:38 -0800

    Checkpoint 46 completed.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,7 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  belongs_to :rating
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
 
 # == Schema Information
